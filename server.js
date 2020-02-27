c3c067c27265c927e82eb6afb452ceb7bb5c628e
Jair-Andrade <61561809+Jair-Andrade@users.noreply.github.com>
2020-02-27 17:18:14 -0300

    Maratona-Dev
    Projeto que realizei em um evento de vídeo aulas realizado pela RocketSeat.

---
+++ server.js
@@ -0,0 +1,78 @@
+// configurando o servidor
+const express = require("express")
+const server = express()
+
+//configurar o servidor para arquivos estaticos
+server.use(express.static('public'))
+
+//habilitar body do formulário
+server.use(express.urlencoded({ extended: true}))
+
+//configurar a conexão com o banco de dados
+const Pool = require('pg').Pool
+const db = new Pool({
+    user: 'postgres',
+    password: '0000',
+    host: 'localhost',
+    port: 5432,
+    database: 'doe'
+})
+
+
+
+//configurando a tempalte engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("./", {
+    express: server,
+    noCache: true,
+})
+//lista de doadores : Vetor ou Array
+
+
+
+
+// configurar a apresentação da página
+server.get("/", function(req, res) {
+
+    db.query("SELECT * FROM donors", function(err, result){
+        
+        if (err) return res.send("Erro de banco de dados.");
+        
+        const donors = result.rows
+        return res.render("index.html", { donors})
+    })
+    
+    
+})
+
+//PEGAR DADOS DO FORMULÁRIO
+server.post("/", function(req, res){
+    const name = req.body.name
+    const email = req.body.email
+    const blood = req.body.blood
+
+    if(name == "" || email == "" || blood == "") {
+        return res.send("Todos os campos são obrigatórios")
+    }
+
+    //coloco valores dentro do banco de dados
+    const query = `
+        INSERT INTO donors("name", "email", "blood")
+        VALUES ($1, $2, $3)`
+    
+    const values = [name, email, blood]
+    
+    db.query(query, values, function(err){
+        //fluxo de erro
+        if(err) return res.send("erro no banco de dados.");
+        //fluxo ideal
+        return res.redirect("./")
+    })
+
+})
+
+
+// ligar o servidor e permitir o acesso na porta 3000
+server.listen(3000, function() {
+    console.log("Iniciei o servidor")
+})
